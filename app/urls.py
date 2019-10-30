050dd5e4502f15b7547f4a6fd15743ffa37d30cf
Clivern <hello@clivern.com>
2019-10-30 21:29:12 +0100

    Fix

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.contrib import admin
 from django.urls import path
 
 # Local Library
